2cc2900738a0901f4292a20490e50384cb03e0a8
Cory Jbara <cjbara@gmial.com>
2016-03-21 17:53:09 -0400

    Daily 13 complete

---
+++ js/d13/scripts/main.js
@@ -0,0 +1,47 @@
+//Name: Cory Jbara
+
+var Item = { 
+	addToDocument: function() {
+		document.body.appendChild(this.item);
+	}
+};
+
+var Label = {
+	createLabel: function(text, id) {
+		this.item = document.createElement("p");
+		this.item.setAttribute("id", id);
+		this.item.innerHTML = text;
+	},
+
+	setText: function(text) {
+		this.item.innerHTML = text;
+	}
+};
+
+var Button = {
+	createButton: function(text, id) {
+		this.item = document.createElement("button");
+		this.item.setAttribute("id", id);
+		this.item.innerHTML = text;
+	},
+
+	addClickEventHandler: function(handler, args) {
+		this.item.onmouseup = function () { handler(args); };
+	}
+};
+
+function changeText(args) {
+	console.log("Changing text");
+	args[1].setText(args[0]);
+};
+
+Label.__proto__ = Item;
+Label.createLabel("Guess Who!", "theLabel");
+
+Button.__proto__ = Item;
+Button.createButton("Click Here", "theButton");
+args = [ "Cory Jbara", Label ];
+Button.addClickEventHandler(changeText, args);
+Button.addToDocument();
+Label.addToDocument();
+
