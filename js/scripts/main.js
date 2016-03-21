26c72c74436293dc81ca49f79da398aa36d20374
Cory Jbara <cjbara@nd.edu>
2016-03-21 02:08:08 -0400

    Homework 12

---
+++ js/scripts/main.js
@@ -0,0 +1,12 @@
+var p = document.createElement("p");
+p.setAttribute("id", "nameLabel");
+var text = document.createTextNode("Who?");
+p.appendChild(text);
+document.body.appendChild(p);
+
+var button = document.getElementById("theButton");
+
+button.onclick = function() {
+  console.log("Clicked");
+  document.getElementById("nameLabel").innerHTML = "Cory Jbara";
+};
